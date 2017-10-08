2b10cf0ff7c895ce5c432475ada1c8145e5ce75e
Robert Vander Ploeg <Vanderploegr1@gmail.com>
2017-10-08 14:05:36 -0400

    Added Files
    Added the files for blog posts 1-3

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+#Don't worry, it gets better!
+print("Hello World...\nAgain \nDo you see what that symbol does?")
+print("New lines can also be created this way!")
+print("But you can't just return")
---
+++ Simple_Math.py
@@ -0,0 +1,45 @@
+# I will use comments to explain code directly in the file!
+# We're going to look at some math characters: + - / * % < > <= >=
+# Each of these does something different
+
+# + does addition
+print(5 + 6) # This will print the solution of the problem
+
+# - does subtraction
+print(1 - 1) # This will simply print the number like above
+
+# / does divition
+print('2.0 / 3.0 =', 2.0 / 3) #only one nubmer needs the .0
+# the '.0' is to make python use floating point (look it up!)
+# using the , we can have multiple things in a single print function
+
+# * is for multiplication
+print('5 * 3 ='',
+        5 * 3)
+# since there is a comma, we can have this print function on multiple lines
+
+# % is the modulo operation
+# this returns the remainder of a division problem
+# 10/3 = 3 with a remainder of 1
+print("The remainder of 10 / 3 is:", 10 % 3) # the answer will be 1
+
+# < is the less than sign
+# if we put in a comparison that is true, it will say true
+# if we put in a comparison that is false, it will print false
+print(3 < 4) # this will print True
+print(4 < 3) # this will print False
+
+# > is the greater than sign
+print(3 > 4)
+print(4 > 3) # How do you think this will return?
+
+# <= >= these signs denote less/greater than OR equal to
+print(2 >= 2)
+print(2 > 2)
+#2 is equal to itself, not greater than itself, so the first statement is true
+# the second is false
+
+# notice that some print commands use "" and others use ''
+# both are valid
+# also, ther is a space between the numbers and operators, this is best pactice
+# 4 + 4 is more readable than 4+4
